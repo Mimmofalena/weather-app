12b47c6d7166439f2c8d31830f2732a145a75691
Mimmofalena <70837839+Mimmofalena@users.noreply.github.com>
2022-01-20 16:38:43 +0100

    added new features, alert error message and restyled

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const btn = document.querySelector(".form button");
-const input = document.querySelector(".form_custom input");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
 const form = document.querySelector(".form");
 
 const cityName = document.querySelector(".display-weather .city");
 const temp = document.querySelector(".display-weather .temperature");
 const wind = document.querySelector(".display-weather .wind");
-const description = document.querySelector(".display-weather .description");
+const humidity = document.querySelector(".display-weather .humidity");
 const weatherForm = document.querySelector(".display-weather");
+const icon = document.querySelector(".icon");
+const flagIcon = document.querySelector(".flag-icon");
+const alertBox = document.querySelector(".alert-box");
+const alertBoxText = document.querySelector(".alert-box_text");
 
 const API_KEY = "1208613478be6d68e54a8bb2e423956d";
 
+const clearInputField = (element) => {
+  element.value = "";
+  element.focus();
+};
+
 btn.addEventListener("click", function (e) {
   e.preventDefault();
+  weatherForm.classList.add("hide");
 
   let url;
   const city = input.value;
@@ -29,26 +39,36 @@ btn.addEventListener("click", function (e) {
       return response.json();
     })
     .then((data) => {
-      const weatherTemperature = data.main.temp;
-      const weatherDescription = data.weather[0].description;
-      const weatherWindSpeed = data.wind.speed;
+      console.log(data.weather[0].icon);
+      console.log(data.sys.country);
 
-      cityName.innerHTML = `City  <strong>${
-        city[0].toUpperCase() + city.slice(1)
-      }</strong>`;
-      temp.innerHTML = `Temperature <strong>${weatherTemperature.toFixed(
-        1
-      )}°C</strong>`;
-      description.innerHTML = `Currently <strong>${
-        weatherDescription[0].toUpperCase() + weatherDescription.slice(1)
-      }</strong>`;
-      wind.innerHTML = `Wind speed <strong>${weatherWindSpeed.toFixed(
-        1
-      )} meter/sec</strong>`;
+      const weatherTemperature = data.main.temp;
+      const weatherHumidity = data.main.humidity;
+      const weatherWindSpeed = data.wind.speed;
+      const weatherIconCode = data.weather[0].icon;
+      const iconUrl = `http://openweathermap.org/img/w/${weatherIconCode}.png`;
+
+      cityName.textContent = ` ${city[0].toUpperCase() + city.slice(1)}`;
+      temp.textContent = `${weatherTemperature.toFixed(1)}°C`;
+      humidity.textContent = ` ${weatherHumidity} %`;
+      wind.textContent = `${weatherWindSpeed.toFixed(1)}mt/sec`;
+
+      const countryCode = data.sys.country;
+      const flagUrl = `https://countryflagsapi.com/svg/${countryCode}`;
+
+      flagIcon.src = flagUrl;
+      icon.src = iconUrl;
 
       weatherForm.classList.remove("hide");
     })
     .catch((err) => {
-      alert(err.message);
+      console.log(err);
+      alertBox.classList.remove("hide");
+      alertBoxText.textContent = `${err.message}, please try again`;
+
+      setTimeout(() => {
+        alertBox.classList.add("hide");
+        clearInputField(input);
+      }, 3000);
     });
 });
